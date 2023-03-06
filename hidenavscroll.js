61f0b9ec42d14375b37e00942629287c07360d14
tanyaterallee <119163691+tanyaterallee@users.noreply.github.com>
2023-03-06 18:11:43 +0100

    Update hidenavscroll.js

--- hidenavscroll.js
+++ hidenavscroll.js
@@ -3,9 +3,9 @@
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
+        document.getElementById("super-navbar__content").style.top = "0";
   } else {
-        document.getElementById("navbar").style.top = "-50px";
+        document.getElementById("super-navbar__content").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 }
